28cd41ab4f014bd70bc72ae411924619797741a7
zay-1-0 <zyad.e.elhabashy2005@gmail.com>
2026-07-22 04:36:05 +0300

    details page done

--- lib/classes/movie.dart
+++ lib/classes/movie.dart
@@ -8,6 +8,7 @@ class Movie{
   late int showID;
   late String title;
   late String imageURL;
+  late String posterURL;
   late double rating;
   late int runTimeStart;
   late int runTimeEnd;
@@ -18,22 +19,27 @@ class Movie{
   late String network;
   late String status;
   late int runtime;
+  bool loaded=false;
 
-  Movie({required this.showID}){
-    setData();
-  }
+  Movie({required this.showID});
 
-  void setData() async{
+  Future<void> setData() async{
 
     Uri url=Uri.https('api.tvmaze.com','shows/1');
     Response response= await get(url);
     Map data= jsonDecode(response.body);
+    
+    Response responseImg=await get(Uri.https('api.tvmaze.com','shows/1/images'));
+    print(responseImg.body);
+
+
 
     title=data['name'];
     imageURL=data['image']['medium'];
+    posterURL=data['image']['original'];
     rating=data['rating']['average'];
     runTimeStart=int.parse(data['premiered'].toString().substring(0,4));
-    runTimeEnd=   (data['ended'].toString().compareTo('null')==0) ?    0      : int.parse(data['ended'].toString().substring(0,4));
+    runTimeEnd=   (data['ended'].toString().compareTo('null')==0) ? 0  : int.parse(data['ended'].toString().substring(0,4));
     genres=List<String>.from(data['genres']);
     summary=data['summary'];
     timeOfShowing= data['schedule']['time'];
@@ -41,6 +47,7 @@ class Movie{
     network=data['network']['name'];
     status= runTimeEnd==0 ? 'Running' : 'Ended';
     runtime = data['runtime'];
+
 
 
 

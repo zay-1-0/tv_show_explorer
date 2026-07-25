c215dd698568b0f3d50f45030404e3f07eed7ae9
zay-1-0 <zyad.e.elhabashy2005@gmail.com>
2026-07-25 14:09:56 +0300

    Added navBar

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ enum CurrentPage{
 class _MyAppState extends State<MyApp> {
 
   int _showID=0;
+  int _selectedPage=0;
 
 
   @override
@@ -41,21 +42,49 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Flutter Demo',
        home: //FavoritesPage()
-      Navigator(
-        pages: [
-          MaterialPage(child: HomePage(didSelectShow: (showID){
+      Scaffold(
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _selectedPage,
+          onDestinationSelected: (int index){
             setState(() {
-              _showID=showID;
+              _selectedPage=index;
             });
-          },)),
+          },
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home_filled), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
+            NavigationDestination(icon: Icon(Icons.favorite), label: 'Favorites'),
+          ],
+        ),
+        body: Navigator(
+          pages: [
+            if(_selectedPage==0)
+            MaterialPage(child: HomePage(didSelectShow: (showID){
+              setState(() {
+                _showID=showID;
+              });
+            },)
+            ),
 
-          if(_showID!=0)
-            MaterialPage(child: DetailWidget(showID: _showID))
-        ],
-        onPopPage: (route,result){
-          _showID=0;
-          return route.didPop(result);
-        },
+            if(_selectedPage==1)
+              MaterialPage(child: Text('Hey')),
+
+            if(_selectedPage==2)
+              MaterialPage(child: FavoritesPage(didSelectShow: (showID){
+                setState(() {
+                  _showID=showID;
+                });
+              },)
+              ),
+
+            if(_showID!=0)
+              MaterialPage(child: DetailWidget(showID: _showID))
+          ],
+          onPopPage: (route,result){
+            _showID=0;
+            return route.didPop(result);
+          },
+        ),
       ),
     );
   }
